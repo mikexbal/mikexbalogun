70b0e7f85fafadeaf9191c3a65c544d596095ab2
Mike <mxbalogun@gmail.com>
2025-12-31 01:05:25 -0500

    Created function to display name

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,6 +1,12 @@
 import './style.css'
 import data from './data.json';
 
-let name = document.getElementById("name");
-name.textContent = data['basic-info'].name;
 
+function displayName(){
+  let name = document.getElementById("name");
+  name.textContent = data['basic-info'].name;
+
+
+}
+
+displayName();
